1b4158bcde6fe39265cad76155830565a73dda50
Ronald SS <ronaldcontact2019@gmail.com>
2021-10-30 20:25:52 -0300

    Exercicios: estrutura-de-repeticao-FOR-RANGE

---
+++ exercicios/estrutura-de-repeticao-FOR-RANGE.py
@@ -0,0 +1,15 @@
+# Etrutura de reptição FOR usando a função RANGE
+
+'''
+range(primeiroElemento, ultimoElemento -1, saltos)
+
+ou apenas um único parametro:
+
+    range(10)
+
+'''
+
+
+
+for i in range(10,21,1):
+    print(i)
