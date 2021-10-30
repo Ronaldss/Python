ea555bd33d5ee01beb57da206bc808e59dc9cb46
Ronald SS <ronaldcontact2019@gmail.com>
2021-10-30 15:09:54 -0300

    Exercícios: Operador-de-atribuicao

---
+++ exercicios/operadores-de-atribuicao.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Cometários
+# Operações matemáticas        + - * / ** %
+# Variáveis
+# Operadores de atribuição     =  +=  -=  *=
+# Operadores relacionais       == != < > <= >=
+# Operadores lógicos           and or not
+
+
+
+
+'''
+OPERADOR            Descrição               Exemplo
+    =               Atribuição              C = A + B atribui o valor de A + B em Cometários
+    +=              Atribuição de soma      C += A equivale a C = C + A
+    -=              Atribuição de sub..     C -= A equivale a C = C - A
+    *=              Atribuição de mul..     C *= A equivale a C = C * A
+'''
