2adc9eb7d697a74eff02477f142a7901423493c5
Ronald SS <ronaldcontact2019@gmail.com>
2021-10-30 20:04:33 -0300

    Exercicios: Estrura-de-repeticao-FOR

---
+++ exercicios/estrutura-de-repeticao-for.py
@@ -0,0 +1,10 @@
+# Estrutura de repetição FOR
+# Percorre o conjunto de dados preestabelecidos
+
+lista1 = [1, 2, 3, 4, 5]
+lista2 = ["Olá", "Ronald", "FOR"]
+lista3 = [0, "name", "Python", 4.23, True]
+
+
+for i in lista1:
+    print(i)
