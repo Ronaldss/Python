fda8ff73df34b98340ce35f8a9307dc253385ab6
Ronald SS <ronaldcontact2019@gmail.com>
2021-10-30 14:03:44 -0300

    Exercícios: Operadores relacionais

---
+++ exercicios/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Cometários
+# Operações matemáticas        + - * / ** %
+# Variáveis
+# OPERADORES RELACIONAIS       == != < > <= >=
+
+
+x = 2 
+y = 3
+
+print(x == y)
+print(x != y)
+print(x < y>)
+print(x > y)
+print(x <= y)
+print(x >= y)
+
+
+
+
