7d0189ff9a6ea942e6d30d37ca668612511aa220
Ronald SS <ronaldcontact2019@gmail.com>
2021-10-31 20:44:50 -0300

    Revisão: operadores de atribuição

---
+++ exercicios/revisao/operadores-de-atribuicao-review.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+# Revisão - Operadores de atribuição
+
+a = 10
+b = 20
+c = 30
+bonos = 100
+
+print('')
+print("Valor original da variavel A: " + str(a))
+print("Valor original da variavel B: " + str(b))
+print("Valor original da variavel C: " + str(c))
+
+print('')
+resA = a + bonos
+print('Variavel A + bonos: ' + str(resA))
+
+print(str(b += c))
+#print('Valor de B+=C: ' + str(resB))
