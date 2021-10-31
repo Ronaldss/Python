2d45cc459fb8d4a91dc6d4be5cb0e9e8736f4570
Ronald SS <ronaldcontact2019@gmail.com>
2021-10-31 14:21:43 -0300

    Revisão: Estrutura de repeticao FOR | RANGE

---
+++ exercicios/revisao/estrutura-de-repeticao-FOR-RANGE-review.py
@@ -0,0 +1,5 @@
+# Estrutura de repetição FOR .RANGE
+
+
+for i in range(10, 21, 2):
+    print(i)
---
+++ exercicios/revisao/estrutura-de-repeticao-for-review.py
@@ -0,0 +1,12 @@
+# Revisão 
+# Estrutara de repetição FOR
+
+lista = [100, 399, 23.2,"Ronald"]
+lista2 = [200, "teste", "coisa", 123]
+
+
+
+for i in lista2:
+    print(i)
+
+
