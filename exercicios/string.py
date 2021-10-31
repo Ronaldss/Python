522dfa5f7ffc5fe2ced6709c2ee50c13896422ad
Ronald SS <ronaldcontact2019@gmail.com>
2021-10-30 21:28:48 -0300

    Exercicio: Navegando entre a posição de string

--- exercicios/string.py
+++ exercicios/string.py
@@ -12,10 +12,26 @@ print(conc)
 '''
 
 
-# Contar quantidade de caracteres de uma string
+''' Contar quantidade de caracteres de uma string
 
 string = "KLHJAKJHFALKJSDLKJSDKHJSDFKH ASDFHJAKLFDSKLJFADNH"
 
 tamanho = len(string)
 
 print(tamanho)
+
+'''
+
+
+#  Navegando entre as posições de uma string
+
+a = "Ronald"
+b = "Emanuella" 
+
+print(a[2])
+print(a[1])
+print(a[0])
+
+
+
+
