b3fb6d817fe891b7dea36d5b0a2db6df5154c6db
Ronald SS <ronaldcontact2019@gmail.com>
2021-10-30 22:12:00 -0300

    Exercicios: string - removendo caracteres especiais do final da string

---
+++ exercicios/string-metodos.py
@@ -0,0 +1,21 @@
+# Alterando a caixa
+
+nome = "Ronald"
+
+# imprimir em minusculo
+print(nome.lower())
+
+
+# impremir em maiusculo
+print(nome.upper())
+
+# a forma original da variavel é mantida
+print(nome)
+
+# Remover caracteres especiais do final da string
+
+nome1 = "Fulano"
+nome2 = "deTal"
+nomeCompleto = nome1 + nome2 + "\n"
+print("Removendo caracteres especiais do final da string:")
+print(nomeCompleto.strip())
