06d14175c17c86a0dd6b4f01d1d53f9dd06be25c
Ronald SS <ronaldcontact2019@gmail.com>
2021-10-30 21:15:02 -0300

    Exercicios: strings, concatenando e contando quantidade de caracteres

---
+++ exercicios/string.py
@@ -0,0 +1,21 @@
+# Trabalhando com string 
+
+''' Concatena string
+
+a = "Ronald"
+b = "Emanuella"
+
+conc = a + " " + b
+
+print(conc)
+
+'''
+
+
+# Contar quantidade de caracteres de uma string
+
+string = "KLHJAKJHFALKJSDLKJSDKHJSDFKH ASDFHJAKLFDSKLJFADNH"
+
+tamanho = len(string)
+
+print(tamanho)
