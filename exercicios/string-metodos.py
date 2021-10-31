06041693dfbc6449c132121cdba6cb3a92840719
Ronald SS <ronaldcontact2019@gmail.com>
2021-10-30 22:19:45 -0300

    Exercicios: Convertendo uma string em uma lista

--- exercicios/string-metodos.py
+++ exercicios/string-metodos.py
@@ -9,13 +9,21 @@ print(nome.lower())
 # impremir em maiusculo
 print(nome.upper())
 
+
 # a forma original da variavel é mantida
 print(nome)
 
-# Remover caracteres especiais do final da string
 
+# Remover caracteres especiais do final da string
 nome1 = "Fulano"
 nome2 = "deTal"
 nomeCompleto = nome1 + nome2 + "\n"
 print("Removendo caracteres especiais do final da string:")
 print(nomeCompleto.strip())
+
+# Convertendo uma lista em uma lista:
+sequencia = "O rato roeu a roupa do rei de Roma"
+
+seq = sequencia.split()
+
+print(seq)
