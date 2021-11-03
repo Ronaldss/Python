fdd3c16d17166a41dd52e7d9729e160448af1ee1
Ronald SS <ronaldcontact2019@gmail.com>
2021-11-02 22:53:04 -0300

    Revisao: String, metodo Replace.

---
+++ exercicios/revisao/string-metodos-replace-review.py
@@ -0,0 +1,7 @@
+# Revisao - String: Metodo REPLACE
+
+minha_frase = "Vamos estudar Javascript durante o final de semana."
+
+minha_frase = minha_frase.replace("Javascript", "Python")
+
+print(minha_frase)
